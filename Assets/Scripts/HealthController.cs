5a0727786f4b17100109c3667c795ddcb94d818b
Aleksander Patschek <aleksander.patschek@hotmail.com>
2020-05-24 14:02:02 +0200

    Add Simple UI with health info

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float health;
+
+    [SerializeField]
+    private float healthLoseOnCollision;
+
+    UISingleton uISingleton;
+
+    void Start()
+    {
+        uISingleton = UISingleton.Instance;
+        uISingleton.setHealth(health);
+    }
+
+    void Update()
+    {
+        if(health <= 0.0f)
+        {
+            uISingleton.showGameOver();
+        }
+    }
+
+    private void OnCollsionWithEnemy(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy" && health > 0)
+        {
+            health = health - healthLoseOnCollision;
+            uISingleton.setHealth(health);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        OnCollsionWithEnemy(collision);
+    }
+
+
+    void OnCollisionStay(Collision collision)
+    {
+        OnCollsionWithEnemy(collision);
+    }
+
+
+   
+}
---
+++ Assets/Scripts/UISingleton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISingleton : MonoBehaviour
+{
+    [SerializeField] 
+    private Text healthLabel;
+
+    [SerializeField]
+    private Text gameOverLabel;
+
+    // Start is called before the first frame update
+    private static UISingleton instance = null;
+
+    // Game Instance Singleton
+    public static UISingleton Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        // if the singleton hasn't been initialized yet
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+        gameOverLabel.enabled = false;
+        
+    }
+
+    public void setHealth(float health)
+    {
+        healthLabel.text = "Health: " + health.ToString();
+    }
+
+    public void showGameOver()
+    {
+        Debug.Log("gameOver");
+        gameOverLabel.enabled = true;
+    }
+}
