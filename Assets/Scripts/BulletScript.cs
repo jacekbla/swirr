d7536ad44efdcacfcb333e5d142e8ce8d658b6a3
Krisov <krzysztof.glos43@gmail.com>
2020-05-25 16:54:25 +0200

    basic shooting

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SelfDestroy", 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SelfDestroy()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnCollsionWithEnemy(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
+        else if (collision.gameObject.tag == "Terrain")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        OnCollsionWithEnemy(collision);
+    }
+
+
+    void OnCollisionStay(Collision collision)
+    {
+        OnCollsionWithEnemy(collision);
+    }
+}
---
+++ Assets/Scripts/PistolScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistolScript : MonoBehaviour
+{
+    public Transform pistolDirection;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 20;
+
+    private Vector3 shift = new Vector3(0, 0.2f, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            GameObject newBullet = Instantiate(bulletPrefab, pistolDirection.position+pistolDirection.forward+shift, pistolDirection.rotation);
+            Rigidbody r = newBullet.GetComponent<Rigidbody>();
+            r.velocity = (pistolDirection.forward) * bulletSpeed;
+        }
+    }
+}
