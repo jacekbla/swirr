5b714af4796c380de8e27315a267c34407b313e3
Krisov <krzysztof.glos43@gmail.com>
2020-05-25 13:51:14 +0200

    basic mob spawn

---
+++ Assets/Scripts/MobCircle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobCircle : MonoBehaviour
+{
+    // Instantiates prefabs in a circle formation
+   public GameObject prefab;
+   public int numberOfObjects = 20;
+   public float radius = 5f;
+   void Start()
+   {
+        InvokeRepeating("SpawnMobs", 0, 60);
+   }
+
+    void SpawnMobs()
+    {
+        for (int i = 0; i < numberOfObjects; i++)
+        {
+            float angle = i * Mathf.PI * 2 / numberOfObjects;
+            float x = Mathf.Cos(angle) * radius;
+            float z = Mathf.Sin(angle) * radius;
+            Vector3 pos = transform.position + new Vector3(x, 10, z);
+            float angleDegrees = -angle * Mathf.Rad2Deg;
+            Quaternion rot = Quaternion.Euler(0, angleDegrees, 0);
+
+            GameObject newMob = (GameObject)Instantiate(prefab, pos, rot);
+            EnemyController enemyController = newMob.GetComponent<EnemyController>();
+            enemyController.target = GameObject.Find("OVRPlayerController").transform;
+
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
